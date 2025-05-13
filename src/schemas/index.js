2444a7d5b420bcf5e67dc24d2dd844dcbba5bce9
Sania Singla <singlasania7717@gmail.com>
2025-05-13 16:26:35 +0530

    done

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,14 +1,13 @@
 import { User, SavedPost, WatchHistory } from './user.Schema.js';
-import { Follower } from '../follower.Schema.js';
-import { Category } from '../category.Schema.js';
-import { Comment } from '../comment.Schema.js';
+import { Follower } from './follower.Schema.js';
+import { Category } from './category.Schema.js';
+import { Comment } from './comment.Schema.js';
 import { PostLike, CommentLike } from './like.Schema.js';
 import { Post, PostView } from './post.Schema.js';
 import { Chat } from './chat.Schema.js';
-import { Message, Attachment } from '../message.Schema.js';
+import { Message, Attachment } from './message.Schema.js';
 import { OnlineUser } from './onlineUser.Schema.js';
 import { Request } from './request.Schema.js';
-
 export {
     User,
     SavedPost,
